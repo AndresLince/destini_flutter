9afee412704bb119c1dfa7194c77f513cf06f2ec
AndresLince <lynxdarmatron@gmail.com>
2023-07-04 05:47:51 -0500

    Use a Flutter Visibility Widget to wrap this FlatButton.

--- lib/main.dart
+++ lib/main.dart
@@ -68,19 +68,20 @@ class _StoryPageState extends State<StoryPage> {
               ),
               Expanded(
                 flex: 2,
-                //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
                 //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-                child: TextButton(
-                  onPressed: () {
-                    setState(() {
-                      storyBrain.nextStory(2);
-                    });
-                  },
-                  child: Text(
-                    storyBrain.getChoice2(),
-                    style: const TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.blue,
+                child: Visibility(
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        storyBrain.nextStory(2);
+                      });
+                    },
+                    child: Text(
+                      storyBrain.getChoice2(),
+                      style: const TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blue,
+                      ),
                     ),
                   ),
                 ),
